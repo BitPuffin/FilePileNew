e93ca8ab397def4a8ea1fa81f3a6d42426da939e
Isak Andersson <MilkshakePanda@live.se>
2011-05-20 21:27:10 +0200

    problem?

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,11 @@
+class PagesController < ApplicationController
+  
+  def main
+    
+  end
+  
+  def register
+    
+  end
+  
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Filepile::Application.config.secret_token = '4aa4a062fa28601dd1a3c4995edfbbb204e4a0a686cb2f2730c522c83644303fc2f3e63d953b6d4e237b64f6b45b3bc7752b7751ee1ecaf4b7d962ead3af432b'
