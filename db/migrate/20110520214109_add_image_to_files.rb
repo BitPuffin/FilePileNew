7a44105464419f911366cd7b74f656934bf587ef
Isak Andersson <MilkshakePanda@live.se>
2011-05-21 01:15:34 +0200

    random push

--- db/migrate/20110520214109_add_image_to_files.rb
+++ db/migrate/20110520214109_add_image_to_files.rb
@@ -1,9 +1,9 @@
 class AddImageToFiles < ActiveRecord::Migration
   def self.up
-    create_table(:files) {|t| t.string :image}
+    create_table(:my_files) {|t| t.string :image}
   end
 
   def self.down
-    drop_table :files
+    drop_table :my_files
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110520214109) do
+ActiveRecord::Schema.define(:version => 20110520222552) do
 
   create_table "files", :force => true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "my_files", :force => true do |t|
     t.string "image"
   end
 
