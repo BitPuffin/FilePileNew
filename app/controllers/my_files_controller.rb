738ef9a22bbfedeaec70f6403d8cb54e968fbd8b
Isak Andersson <MilkshakePanda@live.se>
2011-05-27 15:06:08 +0200

    trolowaefo

--- app/controllers/my_files_controller.rb
+++ app/controllers/my_files_controller.rb
@@ -11,7 +11,7 @@ class MyFilesController < ApplicationController
     @image = MyFile.new(params[:my_file])
       if @image.save
       flash[:notice] = "Image uploaded successfully."
-      redirect_to(:controller => :my_files, :action => :show)
+      redirect_to(:controller => :my_files, :action => :show, :id => @myfile.id)
     else
       render(:action => :new)
     end
